374a4b5cb292d250688a8deb1d02f907a861516f
lanjs <jayce404@foxmail.com>
2026-01-29 11:53:50 +0800

    首次提交

---
+++ src/main/java/org/example/entity/Apple.java
@@ -0,0 +1,11 @@
+package org.example.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Apple {
+    private int weight = 0;
+    private String color = "";
+}
---
+++ src/test/java/org/example/chapt01/FilteringApples.java
@@ -0,0 +1,16 @@
+package org.example.chapt01;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Test;
+
+public class FilteringApples {
+    private final Logger log = LogManager.getLogger(this.getClass());
+
+    @Test
+    public void testFilteringApplesFunction() {
+        log.info("Hello,World");
+    }
+
+
+}
